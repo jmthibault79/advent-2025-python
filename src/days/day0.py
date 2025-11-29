9f25afef6290eed5da961bb199c0f24d8c7cc1d9
Joel Thibault <thibault@broad.dev>
2025-11-29 16:25:28 -0500

    day 0 part 2

--- src/days/day0.py
+++ src/days/day0.py
@@ -1,16 +1,46 @@
 import sys
 from util.input_reader import parse_input_filename, parse_int_list_pair
+from collections import Counter
 
-
-# "day 0" is actually day 1 from 2024
-# the problem involves sorting the right and left lists, and adding up the abs diff between l0,r0 ; l1,r1 ; etc
+# "day 0" is actually day 1 from 2024, for practice
 
 
 def main():
-    file_name = parse_input_filename(sys.argv)
-    a, b = parse_int_list_pair(file_name)
+    a, b = parse(sys.argv)
+    _part1(a, b)
+    _part2(a, b)
+
+
+def parse(argv: list[str]) -> tuple[list[int], list[int]]:
+    file_name = parse_input_filename(argv)
+    return parse_int_list_pair(file_name)
+
+
+def part1():
+    """
+    sort the right and left lists, and add up the abs diff between l0,r0 ; l1,r1 ; etc
+    """
+    a, b = parse(sys.argv)
+    _part1(a, b)
+
+
+def _part1(a: list[int], b: list[int]):
     a.sort()
     b.sort()
+    abs_diffs = [abs(an - b[idx]) for idx, an in enumerate(a)]
+    print(sum(abs_diffs))
 
-    s = sum([abs(an - b[idx]) for idx, an in enumerate(a)])
-    print(s)
+
+def part2():
+    """
+    take the frequency-dist of both lists, and return l0 * freq_l(l0) * freq_r(l0)
+    """
+    a, b = parse(sys.argv)
+    _part2(a, b)
+
+
+def _part2(a: list[int], b: list[int]):
+    a_freq = Counter(a)
+    b_freq = Counter(b)
+    freq_products = [item * a_count * b_freq[item] for item, a_count in a_freq.items()]
+    print(sum(freq_products))
