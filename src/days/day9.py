541785909dca3e5c999f2a162adc5c0be7eb7a74
Joel Thibault <thibault@broad.dev>
2025-12-09 20:50:29 -0500

    d9p2 wd points-inside

--- src/days/day9.py
+++ src/days/day9.py
@@ -1,6 +1,6 @@
 import sys
 from util.input_reader import read_lines
-from itertools import combinations
+from itertools import combinations, groupby
 
 
 def main():
@@ -24,19 +24,41 @@ def parse_input(argv: list[str]) -> list[list[int]]:
 
 
 def rect_area(a: list[int], b: list[int]) -> int:
-    x = abs(a[0] - b[0]) + 1
-    y = abs(a[1] - b[1]) + 1
-    return x * y
+    width = abs(a[0] - b[0]) + 1
+    height = abs(a[1] - b[1]) + 1
+    return width * height
 
 
 def largest_rectangle(points: list[list[int]]) -> int:
-    all_distances = [(rect_area(a, b), a, b) for a, b in combinations(points, 2)]
-    return sorted(all_distances, key=lambda x: x[0])[-1][0]
+    all_areas = [(rect_area(a, b), a, b) for a, b in combinations(points, 2)]
+    return sorted(all_areas, key=lambda x: x[0])[-1][0]
 
 
 def _part1(points: list[list[int]]):
     print(largest_rectangle(points))
 
+def rect_area_without_points_inside(a: list[int], b: list[int], grouped_points: dict[int,list[int]]) -> int:
+    # never mind, this is invalid anyway
+    # there exist some invalid rectangles without points inside
+    # like AB here
+    # A--x
+    # |  | x-B
+    # |  | | |
+    # |  x-x |
+    # x------x
 
-def _part2(input):
-    print("implement me")
+    return 0
+
+def largest_red_green_rectangle(points: list[list[int]]) -> int:
+    sorted_by_row = sorted(points, key=lambda x: x[0])
+    grouped_by_row = {
+        row: sorted([point[1] for point in points])
+        for row, points in groupby(sorted_by_row, key=lambda x: x[0])
+    }
+
+    # for each pair, are there any points inside?  if no: get area 
+    all_areas = [(rect_area_without_points_inside(a, b, grouped_by_row), a, b) for a, b in combinations(points, 2)]
+    return sorted(all_areas, key=lambda x: x[0])[-1][0]
+
+def _part2(points: list[list[int]]):
+    print(largest_red_green_rectangle(points))
--- test/d9_test.py
+++ test/d9_test.py
@@ -1,5 +1,9 @@
 import pytest
-from days.day9 import rect_area, largest_rectangle
+from days.day9 import (
+    rect_area,
+    largest_rectangle,
+    largest_red_green_rectangle,
+)
 
 rect_area_testdata = [
     ([0, 0], [1, 1], 4),
@@ -7,6 +11,16 @@ rect_area_testdata = [
 
 largest_rectangle_testdata = [
     ([[0, 0], [1, 1]], 4),
+]
+
+red_green_testdata = [
+    ([[0, 0], [0, 2], [2, 2], [2, 0]], 9),
+    # 4x3 is wrong here because it includes .
+    # 3x3 is the best we can do in red-green mode
+    # .#X#
+    # ##XX
+    # #XX#
+    ([[0, 1], [0, 3], [2, 3], [2, 0], [1, 0], [1, 1]], 9),
 ]
 
 
@@ -18,3 +32,8 @@ def test_rect_area(a, b, expected):
 @pytest.mark.parametrize("points, expected", largest_rectangle_testdata)
 def test_largest_rectangle(points, expected):
     assert largest_rectangle(points) == expected
+
+
+@pytest.mark.parametrize("points, expected", red_green_testdata)
+def test_largest_red_green_rectangle(points, expected):
+    assert largest_red_green_rectangle(points) == expected
