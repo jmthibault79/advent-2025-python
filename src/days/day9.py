6f4cbdc7fb2ca3914370a273ec064f67df7c9aa4
Joel Thibault <thibault@broad.dev>
2025-12-09 20:50:29 -0500

    d9p2 "filter"
    I found some edge cases where this isn't valid, but it passes!

--- src/days/day9.py
+++ src/days/day9.py
@@ -1,6 +1,7 @@
 import sys
 from util.input_reader import read_lines
 from itertools import combinations
+from collections import defaultdict
 
 
 def main():
@@ -24,19 +25,104 @@ def parse_input(argv: list[str]) -> list[list[int]]:
 
 
 def rect_area(a: list[int], b: list[int]) -> int:
-    x = abs(a[0] - b[0]) + 1
-    y = abs(a[1] - b[1]) + 1
-    return x * y
+    width = abs(a[0] - b[0]) + 1
+    height = abs(a[1] - b[1]) + 1
+    return width * height
 
 
 def largest_rectangle(points: list[list[int]]) -> int:
-    all_distances = [(rect_area(a, b), a, b) for a, b in combinations(points, 2)]
-    return sorted(all_distances, key=lambda x: x[0])[-1][0]
+    all_areas = [(rect_area(a, b), a, b) for a, b in combinations(points, 2)]
+    return sorted(all_areas, key=lambda x: x[0])[-1][0]
 
 
 def _part1(points: list[list[int]]):
     print(largest_rectangle(points))
 
 
-def _part2(input):
-    print("implement me")
+def build_path(red_points: list[list[int]]) -> list[list[int]]:
+    first_point = red_points[0]
+    points_plus_first_again = list(red_points)
+    points_plus_first_again.append(first_point)
+    path = []
+
+    for idx, point in enumerate(points_plus_first_again[1:]):
+        prev_point = red_points[idx]
+        match point, prev_point:
+            case a, b if a[0] == b[0] and a[1] > b[1]:
+                # add path right: [prev_point, point)
+                path.extend([[a[0], col] for col in range(b[1], a[1])])
+            case a, b if a[0] == b[0] and a[1] < b[1]:
+                # add path left: [prev_point, point)
+                path.extend([[a[0], col] for col in range(b[1], a[1], -1)])
+            case a, b if a[1] == b[1] and a[0] > b[0]:
+                # add path up: [prev_point, point)
+                path.extend([[row, a[1]] for row in range(b[0], a[0])])
+            case a, b if a[1] == b[1] and a[0] < b[0]:
+                # add path up: [prev_point, point)
+                path.extend([[row, a[1]] for row in range(b[0], a[0], -1)])
+    return path
+
+
+def point_indices(
+    points: list[list[int]],
+) -> tuple[dict[int, list[int]], dict[int, list[int]]]:
+    """
+    basically a groupby but I got tripped up by semantics so here's a bare-bones version
+    """
+    row_index = defaultdict(list)
+    col_index = defaultdict(list)
+    for row, col in points:
+        row_index[row].append(col)
+        col_index[col].append(row)
+    return {k: sorted(v) for k, v in sorted(row_index.items(), key=lambda x: x[0])}, {
+        k: sorted(v) for k, v in sorted(col_index.items(), key=lambda x: x[0])
+    }
+
+
+def red_green_only(
+    a: list[int],
+    b: list[int],
+    row_index: dict[int, list[int]],
+    col_index: dict[int, list[int]],
+):
+    min_row, max_row = (
+        min(a[0], b[0]),
+        max(a[0], b[0]),
+    )
+    min_col, max_col = (
+        min(a[1], b[1]),
+        max(a[1], b[1]),
+    )
+
+    # check for any strictly-inside points (edges are OK)
+
+    for row in range(min_row + 1, max_row):
+        cols_in_row = row_index.get(row)
+        if cols_in_row is not None:
+            if any([min_col < col < max_col for col in cols_in_row]):
+                return False
+
+    for col in range(min_col + 1, max_col):
+        rows_for_col = col_index.get(col)
+        if rows_for_col is not None:
+            # if there are any strictly-inside points (edges are OK)
+            if any([min_row < row < max_row for row in rows_for_col]):
+                return False
+
+    return True
+
+
+def largest_red_green_rectangle(points: list[list[int]]) -> int:
+    row_index, col_index = point_indices(build_path(points))
+    all_areas = [(rect_area(a, b), a, b) for a, b in combinations(points, 2)]
+
+    for rect_and_area in sorted(all_areas, key=lambda x: x[0], reverse=True):
+        area, a, b = rect_and_area
+        if red_green_only(a, b, row_index, col_index):
+            print(a, b)
+            return area
+    return 0
+
+
+def _part2(points: list[list[int]]):
+    print(largest_red_green_rectangle(points))
--- test/d9_test.py
+++ test/d9_test.py
@@ -1,5 +1,12 @@
 import pytest
-from days.day9 import rect_area, largest_rectangle
+from days.day9 import (
+    rect_area,
+    largest_rectangle,
+    build_path,
+    point_indices,
+    red_green_only,
+    largest_red_green_rectangle,
+)
 
 rect_area_testdata = [
     ([0, 0], [1, 1], 4),
@@ -7,6 +14,43 @@ rect_area_testdata = [
 
 largest_rectangle_testdata = [
     ([[0, 0], [1, 1]], 4),
+]
+path_testdata = [
+    ([[0, 0], [0, 1]], [[0, 0], [0, 1]]),
+    ([[0, 1], [0, 0]], [[0, 1], [0, 0]]),
+    ([[0, 0], [1, 0]], [[0, 0], [1, 0]]),
+    ([[1, 0], [0, 0]], [[1, 0], [0, 0]]),
+    (
+        [[0, 0], [0, 2], [2, 2], [2, 0]],
+        [[0, 0], [0, 1], [0, 2], [1, 2], [2, 2], [2, 1], [2, 0], [1, 0]],
+    ),
+]
+
+point_indices_testdata = [
+    ([[0, 0]], ({0: [0]}, {0: [0]})),
+    (
+        [[0, 1], [0, 3], [2, 3], [2, 0], [1, 0], [1, 1]],
+        ({0: [1, 3], 1: [0, 1], 2: [0, 3]}, {0: [1, 2], 1: [0, 1], 3: [0, 2]}),
+    ),
+]
+
+red_green_only_testdata = [
+    (([0, 0], [1, 1], {0: [0]}, {0: [0]}), True),
+    (
+        ([0, 0], [2, 2], {0: [0, 1, 2], 2: [0, 1, 2]}, {0: [0, 1, 2], 2: [0, 1, 2]}),
+        True,
+    ),
+]
+
+
+red_green_testdata = [
+    ([[0, 0], [0, 2], [2, 2], [2, 0]], 9),
+    # 4x3 is wrong here because it includes .
+    # 3x3 is the best we can do in red-green mode
+    # .#X#
+    # ##XX
+    # #XX#
+    ([[0, 1], [0, 3], [2, 3], [2, 0], [1, 0], [1, 1]], 9),
 ]
 
 
@@ -18,3 +62,23 @@ def test_rect_area(a, b, expected):
 @pytest.mark.parametrize("points, expected", largest_rectangle_testdata)
 def test_largest_rectangle(points, expected):
     assert largest_rectangle(points) == expected
+
+
+@pytest.mark.parametrize("points, expected", path_testdata)
+def test_path(points, expected):
+    assert build_path(points) == expected
+
+
+@pytest.mark.parametrize("points, expected", point_indices_testdata)
+def test_point_indices(points, expected):
+    assert point_indices(points) == expected
+
+
+@pytest.mark.parametrize("inputs, expected", red_green_only_testdata)
+def test_red_green_only(inputs, expected):
+    assert red_green_only(*inputs) == expected
+
+
+@pytest.mark.parametrize("points, expected", red_green_testdata)
+def test_largest_red_green_rectangle(points, expected):
+    assert largest_red_green_rectangle(points) == expected
